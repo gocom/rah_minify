29268ecc54c07c9d3a3c1989190aa4314a3dfb58
Jukka Svahn <web@rahinaa.biz>
2012-02-10 05:16:06 +0200

    Updated comments.

--- config/config.php
+++ config/config.php
@@ -1,29 +1,42 @@
 <?php
 
 /**
- * This is example configuration file for rah_minify.
- * Above should be placed to Textpattern's config file
+ * This is an example configuration file for rah_minify plugin.
+ * Following should be placed to Textpattern's config file
  * (i.e. /textpattern/config.php).
  *
- * Replace all the example filepaths to with real ones.
+ * Replace all the example filepaths to with real ones. Note that
+ * used paths should be absolute (relative can cause problems). 
+ * To build dynamic (i.e. relative) paths, use __FILE__ or txpath constant.
  */
 
-include '/path/to/minify_css_compressor.php';
-include '/path/to/jsmin.php';
-
 /**
- * Sets YUIcompressor.jar's location
+ * Include required libraries, Minify's Minify_CSS_Compressor class 
+ * (css/compress.php) and JSMin-php's JSmin class (jsmin.php).
+ * @link http://code.google.com/p/minify
+ * @link https://github.com/rgrove/jsmin-php
  */
 
-define('rah_minify_yui', '/path/to/yuicompressor.php');
+include '/absolute/path/to/minify_css_compressor.php';
+include '/absolute/path/to/jsmin.php';
 
 /**
- * Sets the files that are compressed.
+ * Path to YUIcompressor.jar
+ * @const rah_minify_yui 
+ * @link https://github.com/yui/yuicompressor/
+ */
+
+define('rah_minify_yui', '/absolute/path/to/yuicompressor.jar');
+
+/**
+ * Sets the files that should be minified/compressed.
+ * @global array $rah_minify
  */
 
 $rah_minify = array(
 	'/path/to/foo.js' => '/path/to/min.foo.js',
 	'/path/to/bar.css' => '/path/to/min.bar.css',
+	'/path/to/hf.less' => '/path/to/min.hf.css',
 );
 
 ?>
